36ee08f14ba553471befc094262b5bf1d6c049e0
ohansemmanuel <ohansemmanuel@outlook.com>
2020-11-10 06:57:14 +0100

    ✨ feat: use plain ol' redux ;)

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,23 +1,29 @@
 import { Cat } from "react-kawaii";
+import { useDispatch, useSelector } from "react-redux";
+import { updateCatMood, MOODS } from ".";
+
 import "./App.css";
 
-const MOODS = [
-  "sad",
-  "shocked",
-  "happy",
-  "blissful",
-  "lovestruck",
-  "excited",
-  "ko",
-];
-
 function App() {
+  const dispatch = useDispatch();
+  const currentMood = useSelector((state = {}) => state.mood);
+
+  const handleMoodUpdate = (evt) => {
+    const mood = evt.target.dataset.type;
+    dispatch(updateCatMood(mood));
+  };
+
   return (
     <div className="App">
-      <Cat size={320} mood="excited" color="#596881" />
+      <Cat size={320} mood={currentMood} color="#596881" />
       <section>
-        {MOODS.map((mood) => (
-          <button data-type={mood} key={mood}>
+        {Object.values(MOODS).map((mood) => (
+          <button
+            data-type={mood}
+            key={mood}
+            className={`${currentMood === mood ? "selected" : ""}`}
+            onClick={handleMoodUpdate}
+          >
             {mood}
           </button>
         ))}
--- basic/src/index.js
+++ basic/src/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "./index.css";
+
+//action type
+export const UPDATE_MOOD = "UPDATE_MOOD";
+
+export const MOODS = {
+  SAD: "sad",
+  SHOCKED: "shocked",
+  HAPPY: "happy",
+  BLISSFUL: "blissful",
+  LOVESTRUCK: "lovestruck",
+  EXCITED: "excited",
+  KO: "ko",
+};
+const INITIAL_STATE = { mood: MOODS.SAD };
+
+//action creators
+export const updateCatMood = (payload) => ({
+  type: UPDATE_MOOD,
+  payload,
+});
+
+//reducer
+export const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UPDATE_MOOD:
+      return { ...state, mood: action.payload };
+
+    default:
+      break;
+  }
+};
+
+//store
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
