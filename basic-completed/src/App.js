fa92008d89d82f3b44bb1ed66fbf67bbdacf01e1
ohansemmanuel <ohansemmanuel@outlook.com>
2021-01-04 08:15:02 +0100

    add basic-completed _ RTK

---
+++ basic-completed/src/App.js
@@ -0,0 +1,35 @@
+import { Cat } from "react-kawaii";
+import { useDispatch, useSelector } from "react-redux";
+import { updateCatMood, MOODS } from ".";
+
+import "./App.css";
+
+function App() {
+  const dispatch = useDispatch();
+  const currentMood = useSelector((state) => state.mood);
+
+  const handleMoodUpdate = (evt) => {
+    const mood = evt.target.dataset.type;
+    dispatch(updateCatMood(mood));
+  };
+
+  return (
+    <div className="App">
+      <Cat size={320} mood={currentMood} color="#596881" />
+      <section>
+        {Object.values(MOODS).map((mood) => (
+          <button
+            data-type={mood}
+            key={mood}
+            className={`${currentMood === mood ? "selected" : ""}`}
+            onClick={handleMoodUpdate}
+          >
+            {mood}
+          </button>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ basic-completed/src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "./index.css";
+
+//action type
+export const UPDATE_MOOD = "UPDATE_MOOD";
+
+export const MOODS = {
+  SAD: "sad",
+  SHOCKED: "shocked",
+  HAPPY: "happy",
+  BLISSFUL: "blissful",
+  LOVESTRUCK: "lovestruck",
+  EXCITED: "excited",
+  KO: "ko",
+};
+const INITIAL_STATE = { mood: MOODS.SAD };
+
+//action creators
+export const updateCatMood = (payload) => ({
+  type: UPDATE_MOOD,
+  payload,
+});
+
+//reducer
+export const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UPDATE_MOOD:
+      return { ...state, mood: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+//store
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
