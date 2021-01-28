018e1e73d97ca9542e2622b7e87b48080288395f
ohansemmanuel <ohansemmanuel@outlook.com>
2021-01-28 07:16:58 +0100

    feat: creating thunk in slice

--- tweetfind/src/features/finder/finderSlice.js
+++ tweetfind/src/features/finder/finderSlice.js
@@ -1,16 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { findTweets } from "./findTweets";
 
-const initialState = { tweets: [] };
+const initialState = { tweets: [], isLoading: false, error: null };
 
 const finderSlice = createSlice({
   name: "finder",
   initialState,
   reducers: {
-    addTweet(state, payload) {
+    loadingTweetsSuccess(state, payload) {
       state.tweets = payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    isLoadingTweets(state) {
+      state.isLoading = true;
+    },
+    loadingTweetsFailed(state, payload) {
+      state.isLoading = false;
+      state.error = payload;
     },
   },
 });
 
-export const { addTweet } = finderSlice.actions;
+export const {
+  addTweets,
+  isLoadingTweets,
+  loadingTweetsSuccess,
+  loadingTweetsFailed,
+} = finderSlice.actions;
 export default finderSlice.reducer;
+
+export const fetchTweets = (searchValue) => async (dispatch) => {
+  try {
+    dispatch(isLoadingTweets());
+    const tweets = await findTweets(searchValue);
+    dispatch(loadingTweetsSuccess(tweets));
+  } catch (error) {
+    dispatch(loadingTweetsFailed(error.toString()));
+  }
+};
