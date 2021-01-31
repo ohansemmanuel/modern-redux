2369c9323b27024698eb4706256df7e7897e7b34
ohansemmanuel <ohansemmanuel@outlook.com>
2021-01-31 23:45:01 +0100

    feat(tweetfind): Thunks and RTK

--- tweetfind/src/features/finder/finderSlice.js
+++ tweetfind/src/features/finder/finderSlice.js
@@ -1,16 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { findTweets } from "./findTweets";
 
-const initialState = { tweets: [] };
+const initialState = { tweets: [], isLoading: false, error: null };
 
 const finderSlice = createSlice({
   name: "finder",
   initialState,
   reducers: {
-    addTweet(state, payload) {
+    loadingTweetsSuccess(state, payload) {
       state.tweets = payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    isLoadingTweets(state) {
+      state.isLoading = true;
     },
   },
 });
 
-export const { addTweet } = finderSlice.actions;
+export const { isLoadingTweets, loadingTweetsSuccess } = finderSlice.actions;
 export default finderSlice.reducer;
+
+export const fetchTweets = (searchValue) => async (dispatch) => {
+  // loading ...
+  dispatch(isLoadingTweets());
+  // perform actual data fetch
+  const tweets = await findTweets(searchValue);
+  // success
+  dispatch(loadingTweetsSuccess(tweets));
+};
