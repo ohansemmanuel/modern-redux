f3285b25cc28f74932ab65f6be8808e5dc65e19f
ohansemmanuel <ohansemmanuel@outlook.com>
2021-01-31 19:35:01 +0100

    feat(tweetfind): sync UI state wit store

--- tweetfind/src/features/numberOfResults/NumberOfResults.jsx
+++ tweetfind/src/features/numberOfResults/NumberOfResults.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Flex, Stack, RadioGroup, Radio, Text } from "@chakra-ui/react";
+import { setNumberOfResults } from "./numberOfResultsSlice";
 
 export const NumberOfResults = () => {
+  const dispatch = useDispatch();
+  const numberOfResults = useSelector((state) => state.numberOfResults);
+
+  const onChange = (val) => {
+    dispatch(setNumberOfResults(val));
+  };
+
   return (
     <Flex my={4} justifyContent="center">
       <Text mr={3}>Number of results:</Text>
-      <RadioGroup defaultValue={10}>
+      <RadioGroup onChange={onChange} value={numberOfResults} defaultValue={10}>
         <Stack spacing={4} direction="row">
           <Radio value="10">10 tweets</Radio>
           <Radio value="15">15 tweets</Radio>
